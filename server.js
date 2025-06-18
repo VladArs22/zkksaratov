ae85a7bcf77d67916b5300ec8760dc950e2874dc
VladArs22 <108784962+VladArs22@users.noreply.github.com>
2025-06-18 19:52:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,10 @@ dotenv.config();
 
 const app = express();
 const pool = new Pool({
-    user: process.env.DB_USER || 'admin',
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'employee_db',
-    password: process.env.DB_PASSWORD || 'secret',
-    port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // нужно для Supabase
+  }
 });
 
 app.use(cors());
